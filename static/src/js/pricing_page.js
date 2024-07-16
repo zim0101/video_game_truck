ee0ff139280bfef66c849e5c09eaf78cf0e36359
expelliarmus05 <arafin1705@cseku.ac.bd>
2024-07-16 14:25:12 +0600

    Update dynamic UI for booking

--- static/src/js/pricing_page.js
+++ static/src/js/pricing_page.js
@@ -1,23 +1,23 @@
 (function () {
     document.addEventListener('DOMContentLoaded', function () {
-        var availabilityButtons = document.querySelectorAll('.check-availability');
+        let availabilityButtons = document.querySelectorAll('.check-availability');
 
         availabilityButtons.forEach(function (button) {
             button.addEventListener('click', function (event) {
                 event.preventDefault(); // Prevent default form submission
 
-                var productId = button.getAttribute('data-product-id');
-                var bookingDateSelector = button.getAttribute('data-booking-date-selector');
-                var numberOfTrucksSelector = button.getAttribute('data-number-of-trucks-selector');
+                let productId = button.getAttribute('data-product-id');
+                let bookingDateSelector = button.getAttribute('data-booking-date-selector');
+                let numberOfTrucksSelector = button.getAttribute('data-number-of-trucks-selector');
 
-                var bookingDate = document.getElementById(bookingDateSelector).value;
-                var numberOfTrucks = document.getElementById(numberOfTrucksSelector).value;
+                let bookingDate = document.getElementById(bookingDateSelector).value;
+                let numberOfTrucks = document.getElementById(numberOfTrucksSelector).value;
 
                 console.log('Product ID:', productId);
                 console.log('Booking Date:', bookingDate);
                 console.log('Number of Trucks:', numberOfTrucks);
 
-                var url = '/fetch_available_slots?' +
+                let url = '/fetch_available_slots?' +
                     'product_id=' + encodeURIComponent(productId) +
                     '&booking_date=' + encodeURIComponent(bookingDate) +
                     '&number_of_trucks=' + encodeURIComponent(numberOfTrucks);
@@ -30,42 +30,50 @@
                         console.log('Availability Response:', data);
 
                         // Clear previous availability messages and checkboxes
-                        var availabilityMessage = document.querySelector('.availability-message');
+                        let availabilityMessage = document.querySelector('.availability-message');
                         availabilityMessage.innerHTML = ''; // Clear previous content
 
                         // Update UI with availability slots
                         if (data.length > 0) {
-                            const formSelector = `form[data-product-id="${productId}"]`;
-                            const form = document.querySelector(formSelector);
-                            console.log(form);
-                            var slotInputName = 'slot_id'; // Assuming slot.id contains a unique identifier
-                            var slotContainer = document.createElement('div');
-
+                            const slotDiv =document.querySelector('div.slot_card');
+                            let slotInputName = 'slot_id'; // Assuming slot.id contains a unique identifier
+                            let container = document.createElement('div');
+                            container.classList.add('col-md-12')
+                            container.classList.add('text-start')
+                            container.classList.add('my-2')
+                            container.classList.add('py-3')
+                            container.setAttribute('role', 'radiogroup'); // Radio button group
+                            container.classList.add('list-group-item')
                             data.forEach(function (slot) {
-                                var container = document.createElement('div');
-                                container.setAttribute('role', 'radiogroup'); // Radio button group
-
-                                var checkbox = document.createElement('input');
+                                let checkbox = document.createElement('input');
                                 checkbox.type = 'radio';
                                 checkbox.name = `product_${productId}_slot`; // Unique name for each product
                                 checkbox.value = slot.id;
                                 checkbox.id = 'slot_' + slot.id;
+                                checkbox.classList.add('mx-2')
 
-                                var label = document.createElement('label');
+                                let label = document.createElement('label');
                                 label.setAttribute('for', 'slot_' + slot.id);
                                 label.textContent = slot.name + ': ' + slot.start_time + ' - ' + slot.end_time;
 
-                                var listItem = document.createElement('ul');
+
+                                let listItem = document.createElement('ul');
+                                if(slot===data[data.length-1]){
+                                    listItem.classList.add('mb-0')
+                                }
                                 listItem.appendChild(checkbox);
                                 listItem.appendChild(label);
                                 container.appendChild(listItem); // Add checkbox and label to container
-                                slotContainer.appendChild(container)
-                                if (form) {
-                                    form.appendChild(slotContainer);
-                                } else {
-                                    console.error('Form element not found.');
-                                }
                             });
+                            if (slotDiv) {
+                                slotDiv.innerHTML=''
+                                slotDiv.appendChild(container);
+                                let submitButton= slotDiv.closest('div.card-body').querySelector('.submit_button')
+                                submitButton.style.display = 'inline-block'
+                                button.style.display = 'None'
+                            } else {
+                                console.error('Form element not found.');
+                            }
                         } else {
                             availabilityMessage.textContent = 'No available slots found.';
                         }
@@ -78,6 +86,30 @@
                         // Handle error display if needed
                     });
             });
+        });
+        //code for reversing
+        let inputFields = document.querySelectorAll('.input');
+        
+        inputFields.forEach(function (input){
+            input.addEventListener('change',function (){
+                let slotCardHTML='<div class ="col-md-12 py-3 my-2 list-group-item">'
+                slotCardHTML += '<p class="mt-1 mb-n1"><strong>'
+                slotCardHTML += 'Please select the Booking Date, </br>'
+                slotCardHTML += 'Number of Trucks & Click the </br>'
+                slotCardHTML += 'Check Availability Button see </br>'
+                slotCardHTML += 'the available slots'
+                slotCardHTML += '</strong></p>'
+                slotCardHTML += '</div>'
+
+                let slotCard = input.closest('.card-body').querySelector('.slot_card');
+                let submitButton = input.closest('.card-body').querySelector('.submit_button');
+                let checkButton = input.closest('.card-body').querySelector('.check-availability');
+                if(checkButton && submitButton && slotCard){
+                    slotCard.innerHTML=slotCardHTML
+                    submitButton.style.display = 'None'
+                    checkButton.style.display = 'inline-block'
+                }
+            })
         });
     });
 })();
